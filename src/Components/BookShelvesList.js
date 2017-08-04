6093d3aeae04cdc316090e8caae092e879685c26
Niko Yakimov <nyakimov@gmail.com>
2017-08-04 10:03:56 -0400

    feat: Add BookShelvesList component
    The BookShelvesList component takes the master books array and
    creates a separate BookShelf with the respective subset of Books

---
+++ src/Components/BookShelvesList.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import BookShelf from './BookShelf'
+
+class BookShelvesList extends Component {
+    static propTypes = {
+        books: PropTypes.array,
+        onShelfChange: PropTypes.func.isRequired
+    }
+
+    render () {
+        const {books} = this.props
+        let bookShelves = [
+            {
+                name: "Currently Reading",
+                books: books.filter(book => book.shelf === "currentlyReading")
+            },
+            {
+                name: "Want To Read",
+                books: books.filter(book => book.shelf === "wantToRead")
+            },
+            {
+                name: "Read",
+                books: books.filter(book => book.shelf === "read")
+            },
+        ]
+        
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        {bookShelves.map((shelf, index) => (
+                            <BookShelf 
+                                key={index}
+                                shelfName={shelf.name}
+                                books={shelf.books}
+                                onShelfChange={this.props.onShelfChange}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="open-search">
+                    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BookShelvesList
