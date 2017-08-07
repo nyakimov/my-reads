f0ff3411a5db409d8d8f06d503cfbcf63d1c7101
Niko Yakimov <nyakimov@gmail.com>
2017-08-07 09:30:41 -0400

    fix: Bug fix for missing imageLinks or authors from the Book object

--- src/components/Book.js
+++ src/components/Book.js
@@ -9,11 +9,12 @@ class Book extends Component {
 
     render() {
         let {book} = this.props
+        const imageURL = book.imageLinks ? book.imageLinks.thumbnail || book.imageLinks.smallThumbnail : ""
         return(
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${imageURL}")` }}></div>
                         <div className="book-shelf-changer">
                         <select onChange={(e) => this.props.onShelfChange(book, e.target.value)} value={book.shelf}>
                             <option value="none" disabled>Move to...</option>
@@ -25,7 +26,7 @@ class Book extends Component {
                         </div>
                     </div>
                     <div className="book-title">{book.title}</div>
-                      {book.authors.map((author, index) => (
+                      {book.authors && book.authors.map((author, index) => (
                         <div key={index} className="book-authors">
                             {author}
                         </div>
