0a2c582e54a2eb735040964d5618eda8d56570aa
Niko Yakimov <nyakimov@gmail.com>
2017-08-07 12:22:55 -0400

    feat: Implement the searchBooks function and displaying of the foundBooks

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,10 +1,47 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import Book from './Book'
+import * as BooksAPI from '../utils/BooksAPI'
 
 class BookSearch extends Component {
+    static propTypes = {
+        books: PropTypes.array,
+        onShelfChange: PropTypes.func.isRequired
+    }
 
-    render(){
+    state = {
+        query: '',
+        foundBooks: []
+    }
+
+    searchBooks = (query) => {
+        if (query !== "") {
+            BooksAPI.search(query, 20).then((books) => {
+                //Handle the error response
+                if (!books.error) {
+                    this.setState(state => ({
+                        foundBooks: books
+                    }))
+                }
+            })
+        }
+        //Empty the list of books if the search bar is an empty string
+        else {
+            this.setState(state => ({
+                foundBooks: []
+            }))
+        }
+            
+    }
+
+    updateQuery = (query) => {
+        this.setState({query})
+        this.searchBooks(query)
+    }
+
+    render() {
         return(
           <div className="search-books">
                 <div className="search-books-bar">
@@ -18,13 +55,24 @@ class BookSearch extends Component {
                             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                             you don't find a specific author or title. Every search is limited by search terms.
                             */}
-                            <input type="text" placeholder="Search by title or author"/>
+                            <input 
+                                type='text'
+                                placeholder='Search by title or author'
+                                value={this.state.query}
+                                onChange={(e) => this.updateQuery(e.target.value)}
+                            />
                             
                         </div>
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
-
+                        {this.state.foundBooks.map(book => (
+                            <Book 
+                                key={book.id}
+                                book={book}
+                                onShelfChange={this.props.onShelfChange}
+                            />
+                        ))}
                     </ol>
                 </div>
           </div>
